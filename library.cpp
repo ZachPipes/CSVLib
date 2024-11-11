cc9a84d0ef549b6bffa7a046601d5dfb872b15a6
Zachery Pipes <zacherypipes@gmail.com>
2024-11-10 18:32:18 -0600

    Created project, started writing the parse and print functions

---
+++ library.cpp
@@ -0,0 +1,19 @@
+#include "library.h"
+
+#include <string>
+#include <vector>
+
+// Create headers as strings that are equal to 0, 1, 2, 3, etc
+
+template <FILE* input, typename... Args>
+std::vector<std::string> parse(FILE* inputFile, Args&&... args) {
+    std::vector<std::string> parsedFile;
+
+
+
+    return parsedFile;
+}
+
+void print(std::vector<std::string> inputFile) {
+
+}
---
+++ library.h
@@ -0,0 +1,12 @@
+#ifndef CSV_LIBRARY_H
+#define CSV_LIBRARY_H
+
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT
+#endif
+
+EXPORT // UNFUNISINUSHED
+
+#endif //CSV_LIBRARY_H
